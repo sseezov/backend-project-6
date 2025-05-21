395c3c87a0e8e2a78c661580a659531af88be245
sseezov <sseezov@mail.ru>
2025-05-22 00:22:34 +0700

    init

--- server/plugin.js
+++ server/plugin.js
@@ -1,17 +1,6 @@
-import Fastify from 'fastify'
-const fastify = Fastify({
-  logger: true
-})
-
-// Declare a route
-fastify.get('/', async function handler (request, reply) {
-  return { hello: 'world' }
-})
-
-// Run the server!
-try {
-  await fastify.listen({ port: 3000 })
-} catch (err) {
-  fastify.log.error(err)
-  process.exit(1)
-}
+export default (app) => {
+  app
+    .get('/', { name: 'root' }, (req, reply) => {
+      reply.send('hello')
+    })
+};
