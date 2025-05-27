ef365d202dc9c8b550d33527aea34889a349a5e7
sseezov <sseezov@mail.ru>
2025-05-28 00:06:03 +0700

    setup pug

--- server/plugin.js
+++ server/plugin.js
@@ -1,6 +1,31 @@
+import fastifyView from '@fastify/view';
+import Pug from 'pug';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(path.dirname(import.meta.url));
+
+const setUpViews = (app) => {
+  app.register(fastifyView, {
+    engine: {
+      pug: Pug,
+    },
+    includeViewExtension: true,
+    templates: path.join(__dirname, '..', 'server', 'views'),
+  });
+
+  // app.decorateReply('render', function render(viewPath, locals) {
+  //   this.view(viewPath, { ...locals, reply: this });
+  // });
+};
+
 export default (app) => {
+  setUpViews(app);
+
   app
     .get('/', { name: 'root' }, (req, reply) => {
-      reply.send('hello')
+      reply.view('/header')
     })
+
+  return app;
 };
