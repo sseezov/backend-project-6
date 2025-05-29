8ebb1a05726f162e9583fe9a0101fa6569c37967
sseezov <sseezov@mail.ru>
2025-05-29 23:51:33 +0700

    step2 done

---
+++ server/assets/main.js
@@ -0,0 +1,10 @@
+// import i18n from 'i18next';
+
+// const i18nextInstance = i18n.createInstance();
+// i18nextInstance.init({
+//   lng: state.language,
+//   resources,
+// });
+// // 
+
+// console.log(1)
--- server/plugin.js
+++ server/plugin.js
@@ -1,16 +1,39 @@
-import fastifyView from '@fastify/view';
-import Pug from 'pug';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fastifyView from '@fastify/view';
+import Pug from 'pug';
+import i18next from 'i18next';
+import getHelpers from './helpers/index.js';
+
+import ru from './locales/ru.js';
+import en from './locales/en.js';
 
 const __dirname = fileURLToPath(path.dirname(import.meta.url));
 
+const setupLocalization = async () => {
+  await i18next
+    .init({
+      lng: 'en',
+      fallbackLng: 'ru',
+      // debug: isDevelopment,
+      resources: {
+        ru,
+        en,
+      },
+    });
+};
+
 const setUpViews = (app) => {
+  const helpers = getHelpers(app);
   app.register(fastifyView, {
     engine: {
       pug: Pug,
     },
     includeViewExtension: true,
+    defaultContext: {
+      ...helpers,
+      assetPath: (filename) => `/assets/${filename}`,
+    },
     templates: path.join(__dirname, '..', 'server', 'views'),
   });
 
@@ -19,7 +42,8 @@ const setUpViews = (app) => {
   // });
 };
 
-export default (app) => {
+export default async (app, _options) => {
+  await setupLocalization();
   setUpViews(app);
 
   app
